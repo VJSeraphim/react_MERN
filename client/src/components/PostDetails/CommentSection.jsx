c01f1a505e31c0dea4bbc21722242e2f5bf36568
VJS <minsung_96@naver.com>
2021-06-30 23:50:48 +0900

    Comment Section 2

--- client/src/components/PostDetails/CommentSection.jsx
+++ client/src/components/PostDetails/CommentSection.jsx
@@ -4,10 +4,28 @@ import {useDispatch} from 'react-redux'
 
 import useStyles from './styles'
 
-const CommentSection = () => {
+const CommentSection = ({post}) => {
+    const classes = useStyles()
+    const [comments, setComments] = useState([1, 2, 3, 4])
+    const [comment, setComment] = useState('')
     return (
         <div>
-            
+            <div className = {classes.commentsOuterContainer}>
+                <div className = {classes.commentsInnerContainer}>
+                    <Typography gutterBottom variant="h6"></Typography>
+                    {comments.map((c, i) => (
+                        <Typography key = {i} gutterBottom variant="subtitle1">
+                            Comment Section {i}
+                        </Typography>
+                    ))}
+                </div>
+                <div style={{width: '70%'}}>
+                    <Typography gutterBottom variant="h6">
+                        Write a Comment...
+                    </Typography>
+                    <TextField fullWidth rows={4} variant="outlined" label="Comment" multiline value={comment} onChange={(e) => setComment(e.target.value)}/>
+                </div>
+            </div>
         </div>
     )
 }
